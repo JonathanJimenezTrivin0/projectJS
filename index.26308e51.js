382c4656a22797e162660a938995920e15454ef4
Hynth <cdar93@gmail.com>
2023-08-17 01:42:56 +0000

    Deploying to gh-pages from @ JonathanJimenezTrivin0/projectJS@9515489b2df6b51c8b864dbea7c12cdb64c1b890 🚀

---
+++ index.26308e51.js
@@ -0,0 +1,2 @@
+!function(){var e=document.getElementById("myModal"),n=document.getElementById("openModalBtn"),t=document.getElementsByClassName("close")[0];n.addEventListener("click",(function(){e.style.display="block"})),t.addEventListener("click",(function(){e.style.display="none"})),window.addEventListener("click",(function(n){n.target===e&&(e.style.display="none")}))}();
+//# sourceMappingURL=index.26308e51.js.map
---
+++ index.813c698e.js
@@ -0,0 +1,2 @@
+const e=document.getElementById("myModal"),t=document.getElementById("openModalBtn"),n=document.getElementsByClassName("close")[0];t.addEventListener("click",(()=>{e.style.display="block"})),n.addEventListener("click",(()=>{e.style.display="none"})),window.addEventListener("click",(t=>{t.target===e&&(e.style.display="none")}));
+//# sourceMappingURL=index.813c698e.js.map
