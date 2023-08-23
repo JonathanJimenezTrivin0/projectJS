238366f5048b3b48f3a53dbf55d857f88f8a1627
Hynth <cdar93@gmail.com>
2023-08-23 01:56:06 +0000

    Deploying to gh-pages from @ JonathanJimenezTrivin0/projectJS@926a0edd549f8a71b7640f330e2bd6a29f4a573b 🚀

---
+++ index.8a8a06c8.js
@@ -0,0 +1,2 @@
+!function(){let e=document.getElementById("myModal"),t=document.getElementById("openModalBtn"),n=document.getElementsByClassName("close")[0];t.addEventListener("click",()=>{e.style.display="block"}),n.addEventListener("click",()=>{e.style.display="none"}),window.addEventListener("click",t=>{t.target===e&&(e.style.display="none")})}();
+//# sourceMappingURL=index.8a8a06c8.js.map
